6620ce4bb01b6b73490648e5f869019acf806cb4
Hannes Oswald <hannes.oswald@prolion.com>
2024-07-20 11:25:35 +0200

    register event loop group for new channel

--- src/main/java/com/emc/ecs/nfsclient/network/Connection.java
+++ src/main/java/com/emc/ecs/nfsclient/network/Connection.java
@@ -73,6 +73,8 @@ public class Connection {
      */
     private final Bootstrap _clientBootstrap;
 
+    private final NioEventLoopGroup _eventLoopGroup;
+
     /**
      * Netty channel representing a tcp connection.
      */
@@ -141,8 +143,8 @@ public class Connection {
         _port = port;
         _usePrivilegedPort = usePrivilegedPort;
         _clientBootstrap = new Bootstrap();
-        EventLoopGroup workerGroup = new NioEventLoopGroup();
-        _clientBootstrap.group(workerGroup);
+        _eventLoopGroup = new NioEventLoopGroup();
+        _clientBootstrap.group(_eventLoopGroup);
         _clientBootstrap.channel(NioSocketChannel.class);
         // Configure the client.
         _clientBootstrap.remoteAddress(new InetSocketAddress(_remoteHost, _port));
@@ -394,6 +396,7 @@ public class Connection {
             try {
                 Channel channel = _clientBootstrap.config().channelFactory().newChannel();
                 channel.config().setOptions(_clientBootstrap.config().options());
+                _eventLoopGroup.register(channel).awaitUninterruptibly();
                 ChannelFuture bindFuture = channel.bind(new InetSocketAddress(port)).awaitUninterruptibly();
                 if (bindFuture.isSuccess()) {
                     System.out.println("Success! Bound to port " + port);
